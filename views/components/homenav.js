c057224d30a326f7101654c29babd93b315fe4f7
ElvisLC <elvislacruz22@gmail.com>
2024-07-10 22:29:55 -0400

    portfolio

--- views/components/homenav.js
+++ views/components/homenav.js
@@ -8,7 +8,7 @@ const crearBarraNav = () => {
     navBar.innerHTML = `<img class="logoImg" src="/media/logo.jpeg" width="150 " height="150" alt="Logo de la lavandería">
         <div id="mySidenav" class="sidenav">
             <span class="closebtn btndisable" onclick="closeNav()">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="32" height="32" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="60px" height="60px" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                     <path d="M18 6l-12 12" />
                     <path d="M6 6l12 12" />
@@ -23,7 +23,7 @@ const crearBarraNav = () => {
           
         <span onclick="openNav()" class="menuBtn">
             
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-menu-2" width="32" height="32" viewBox="0 0 24 24" stroke-width="1.5" stroke="#053968" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-menu-2" width="60px" height="60px" viewBox="0 0 24 24" stroke-width="1.5" stroke="#053968" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
             <path d="M4 6l16 0" />
             <path d="M4 12l16 0" />
